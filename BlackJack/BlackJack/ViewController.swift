64d2d95c9f76f70b9069b46cc57e30f40b330020
Lauren Small <lauren.small@detroitlabs.com>
2018-08-28 10:17:45 -0400

    added deck of Cards array of tuples

--- BlackJack/BlackJack/ViewController.swift
+++ BlackJack/BlackJack/ViewController.swift
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    
+    let deckOfCards = [(face: "diamonds", value: 2), (face: "hearts", value: 2), (face: "clubs", value: 2), (face: "spades", value: 2), (face: "diamonds", value: 3), (face: "hearts", value: 3), (face: "clubs", value: 3), (face: "spades", value: 3), (face: "diamonds", value: 4), (face: "hearts", value: 4), (face: "clubs", value: 4), (face: "spades", value: 4), (face: "diamonds", value: 5), (face: "hearts", value: 5), (face: "clubs", value: 5), (face: "spades", value: 5), (face: "diamonds", value: 6), (face: "hearts", value: 6), (face: "clubs", value: 6), (face: "spades", value: 6), (face: "diamonds", value: 7), (face: "hearts", value: 7), (face: "clubs", value: 7), (face: "spades", value: 7), (face: "diamonds", value: 8), (face: "hearts", value: 8), (face: "clubs", value: 8), (face: "spades", value: 8), (face: "diamonds", value: 9), (face: "hearts", value: 9), (face: "clubs", value: 9), (face: "spades", value: 9),(face: "diamonds", specialCards: "jack", value: 10), (face: "hearts", specialCards: "jack", value: 10), (face: "clubs", specialCards: "jack", value: 10), (face: "spades", specialCards: "jack", value: 10),(face: "diamonds", specialCards: "queen", value: 10), (face: "hearts", specialCards: "queen", value: 10), (face: "clubs", specialCards: "queen", value: 10), (face: "spades", specialCards: "queen", value: 10),(face: "diamonds", specialCards: "king", value: 10), (face: "hearts", specialCards: "king", value: 10), (face: "clubs", specialCards: "king", value: 10), (face: "spades", specialCards: "king", value: 10), (face: "diamonds", specialCards: "ace", valueOne: 1, valueTwo: 11), (face: "hearts", specialCards: "ace", valueOne: 1, valueTwo: 11), (face: "clubs", specialCards: "ace", valueOne: 1, valueTwo: 11), (face: "spades", specialCards: "jack", valueOne: 1, valueTwo: 11),] as [Any]
+    
+    
 }
 
