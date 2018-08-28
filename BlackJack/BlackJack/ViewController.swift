f912d66c8509a36acf2252163b6314ba11a0bd62
Lauren Small <lauren.small@detroitlabs.com>
2018-08-28 11:01:44 -0400

    added random card generation

--- BlackJack/BlackJack/ViewController.swift
+++ BlackJack/BlackJack/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        getRandom()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -26,5 +27,30 @@ class ViewController: UIViewController {
     let deckOfCards = [(face: "diamonds", value: 2), (face: "hearts", value: 2), (face: "clubs", value: 2), (face: "spades", value: 2), (face: "diamonds", value: 3), (face: "hearts", value: 3), (face: "clubs", value: 3), (face: "spades", value: 3), (face: "diamonds", value: 4), (face: "hearts", value: 4), (face: "clubs", value: 4), (face: "spades", value: 4), (face: "diamonds", value: 5), (face: "hearts", value: 5), (face: "clubs", value: 5), (face: "spades", value: 5), (face: "diamonds", value: 6), (face: "hearts", value: 6), (face: "clubs", value: 6), (face: "spades", value: 6), (face: "diamonds", value: 7), (face: "hearts", value: 7), (face: "clubs", value: 7), (face: "spades", value: 7), (face: "diamonds", value: 8), (face: "hearts", value: 8), (face: "clubs", value: 8), (face: "spades", value: 8), (face: "diamonds", value: 9), (face: "hearts", value: 9), (face: "clubs", value: 9), (face: "spades", value: 9),(face: "diamonds", specialCards: "jack", value: 10), (face: "hearts", specialCards: "jack", value: 10), (face: "clubs", specialCards: "jack", value: 10), (face: "spades", specialCards: "jack", value: 10),(face: "diamonds", specialCards: "queen", value: 10), (face: "hearts", specialCards: "queen", value: 10), (face: "clubs", specialCards: "queen", value: 10), (face: "spades", specialCards: "queen", value: 10),(face: "diamonds", specialCards: "king", value: 10), (face: "hearts", specialCards: "king", value: 10), (face: "clubs", specialCards: "king", value: 10), (face: "spades", specialCards: "king", value: 10), (face: "diamonds", specialCards: "ace", valueOne: 1, valueTwo: 11), (face: "hearts", specialCards: "ace", valueOne: 1, valueTwo: 11), (face: "clubs", specialCards: "ace", valueOne: 1, valueTwo: 11), (face: "spades", specialCards: "jack", valueOne: 1, valueTwo: 11),] as [Any]
     
     
+    
+    @objc func getRandom() {
+//        let randomCard = Int(arc4random() % UInt32(deckOfCards.count))
+        let randomCard = Int(arc4random_uniform(UInt32(deckOfCards.count)))
+        let theCard = deckOfCards[randomCard]
+        print(theCard)
+        
+//        let randomCard = Int(arc4random_uniform(52))
+//        let suit = randomCard / 13 + 1
+//        let value = randomCard % 13 + 1
+        
+        
+//        let suit = Int(arc4random_uniform(4)) + 1
+//        let value = Int(arc4random_uniform(13)) + 1
+    
 }
+    
+    func removeCardFromDeck() {
+        
+    }
+    
+    func drawTwoCards() {
+        getRandom()
+        getRandom()
+    }
 
+}
